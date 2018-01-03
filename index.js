a96b7b8ed250a19ee0cf8fc819a7378c00925597
niyazh6 <35020627+niyazh6@users.noreply.github.com>
2018-01-03 19:04:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.on("ready",function(){
 bot.on("message",function(message)
 {    console.log(message.content);
     if (message.author.equals(bot.user)) return;
-    if(message.content == "hello" || message.content == "Hello" || message.content == (bot.user.toString()) + "hello")
+    if(message.content == "hello" || message.content == "Hello" || message.content == "@" + (bot.user.toString()) + "hello")
       message.channel.sendMessage(message.author.toString() + " hi..there");
     if(!message.content.startsWith(prefix)) return;
    
